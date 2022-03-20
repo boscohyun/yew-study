16c8da4edc26139499ca4094b3578df7e42ad13c
Hyun Seungmin <boscohyun@gmail.com>
2022-03-21 01:06:39 +0900

    implement input and output

--- src/seonbi.rs
+++ src/seonbi.rs
@@ -1,26 +1,61 @@
 use std::rc::Rc;
 use tracing::info;
-use yew::{function_component, html, use_reducer, Callback, MouseEvent, Properties, Reducible};
+use wasm_bindgen::JsCast;
+use web_sys::HtmlTextAreaElement;
+use yew::{
+    function_component, html, use_reducer, Callback, Event, MouseEvent, Properties, Reducible,
+};
 
 #[derive(Debug, Properties, PartialEq)]
 struct InputProps {
+    interactable: bool,
     on_update_content: Option<Callback<String>>,
 }
 
 #[function_component(Input)]
 fn input(props: &InputProps) -> Html {
+    let onchange = if let Some(on_update_content) = props.on_update_content.to_owned() {
+        Callback::from(move |e: Event| {
+            if let Some(textarea) = e
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlTextAreaElement>().ok())
+            {
+                on_update_content.emit(textarea.value())
+            }
+        })
+    } else {
+        Callback::default()
+    };
+
     html!(
         <>
             <div>{ "Input here!" }</div>
+            <textarea
+                rows="10"
+                cols="20"
+                placeholder="Write something here!"
+                {onchange}
+                readonly={!props.interactable.to_owned()} />
         </>
     )
 }
 
+#[derive(Debug, Properties, PartialEq)]
+struct OutputProps {
+    content: String,
+}
+
 #[function_component(Output)]
-fn output() -> Html {
+fn output(props: &OutputProps) -> Html {
+    let content = props.content.to_owned();
     html!(
         <>
             <div>{ "Output here!" }</div>
+            <textarea
+                rows="10"
+                cols="20"
+                readonly=true
+                value={content} />
         </>
     )
 }
@@ -36,18 +71,23 @@ fn adjustment_options() -> Html {
 
 #[derive(Debug, Properties, PartialEq)]
 struct AdjustmentButtonProps {
+    interactable: bool,
     onclick_adjustment_button: Option<Callback<MouseEvent>>,
 }
 
 #[function_component(AdjustmentButton)]
 fn adjustment_button(props: &AdjustmentButtonProps) -> Html {
-    let onclick = if let Some(onclick) = props.onclick_adjustment_button.clone() {
+    let onclick = if let Some(onclick) = props.onclick_adjustment_button.to_owned() {
         onclick
     } else {
         Callback::default()
     };
     html!(
-        <button {onclick} >{ "Adjust" }</button>
+        <button
+            {onclick}
+            disabled={!props.interactable.to_owned()}>
+            { "Adjust" }
+        </button>
     )
 }
 
@@ -68,6 +108,7 @@ impl Default for SeonbiState {
 }
 
 enum SeonbiAction {
+    UpdateInputContent(String),
     ClickAdjustmentButton,
 }
 
@@ -76,9 +117,20 @@ impl Reducible for SeonbiState {
 
     fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
         match action {
+            Self::Action::UpdateInputContent(input_content) => Self {
+                interactable: self.interactable,
+                input_content,
+                output_content: self.output_content.to_owned(),
+            }
+            .into(),
             Self::Action::ClickAdjustmentButton => {
-                info!("adjustment button clicked!");
-                self
+                let output_content = self.input_content.clone();
+                Self {
+                    interactable: false,
+                    input_content: self.input_content.to_owned(),
+                    output_content,
+                }
+                .into()
             }
         }
     }
@@ -86,7 +138,14 @@ impl Reducible for SeonbiState {
 
 #[function_component(Seonbi)]
 pub fn seonbi() -> Html {
+    info!("render seonbi!");
     let reducer = use_reducer(SeonbiState::default);
+    let on_update_input_content = {
+        let reducer = reducer.clone();
+        Callback::from(move |content: String| {
+            reducer.dispatch(SeonbiAction::UpdateInputContent(content))
+        })
+    };
     let onclick_adjustment_button = {
         let reducer = reducer.clone();
         Callback::from(move |_| reducer.dispatch(SeonbiAction::ClickAdjustmentButton))
@@ -94,10 +153,14 @@ pub fn seonbi() -> Html {
     html!(
         <>
             <div>{ "Hello, Seonbi!" }</div>
-            <Input />
-            <Output />
+            <Input
+                interactable={reducer.interactable.clone()}
+                on_update_content={ on_update_input_content } />
+            <Output content={ reducer.output_content.clone() } />
             <AdjustmentOptions />
-            <AdjustmentButton {onclick_adjustment_button} />
+            <AdjustmentButton
+                interactable={reducer.interactable.clone()}
+                {onclick_adjustment_button} />
         </>
     )
 }
